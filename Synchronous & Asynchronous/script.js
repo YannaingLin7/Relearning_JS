e41810545b0ffe5e681c6ef4620abb85ec62ed63
YannaingLin-w2hf <yannainglin.dev@gmail.com>
2023-09-15 00:08:59 +0630

    updated SYNC & ASYNC

--- Synchronous & Asynchronous/script.js
+++ Synchronous & Asynchronous/script.js
@@ -7,11 +7,11 @@
 const login = function () {
   const loginWithPromise = new Promise(function (resolve, reject) {
     setTimeout(() => {
-      resolve("Login success");               // parameter ( resolve()/reject() ) ပေါ်တွင်မူတည်၍ then()/catch() ကို ဖမ်းမည်။
+      resolve("Login success"); // parameter ( resolve()/reject() ) ပေါ်တွင်မူတည်၍ then()/catch() ကို ဖမ်းမည်။
     }, 2000);
   });
 
-  return loginWithPromise;                    // This function return a promise
+  return loginWithPromise; // This function return a promise
 };
 
 const fetchData = function () {
@@ -26,44 +26,60 @@ const fetchData = function () {
 const getUserPhoto = function () {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      // reject("ERROR in fetching Photo!");
-      resolve("Photo has been fetched.");
+      reject("ERROR in fetching Photo!");
+      // resolve("Photo has been fetched.");
     }, 2000);
   });
 };
 
-const getUserFriends = function() {
-  return new Promise ((resolve, reject) => {
+const getUserFriends = function () {
+  return new Promise((resolve, reject) => {
     setTimeout(() => {
-      resolve("User friends have been fetched.")
+      resolve("User friends have been fetched.");
     }, 2000);
   });
-}
+};
 
 const showUI = function (data) {
   console.log("Showing UI with user data... : ", data);
 };
 
+const buildUI = async function () {
+  const loginData = await login();
+  console.log(loginData);
+  const dataFromFetchData = await fetchData();
+  console.log(dataFromFetchData);
+  const userPhoto = await getUserPhoto();
+  console.log(userPhoto);
+  const userFriends = await getUserFriends();
+  console.log(userFriends);
+  showUI(userFriends);
+};
+
+buildUI().catch((error) => {
+  console.log("Inside catch :: ", error)
+});
 
 // promise chainging
 // promise chainging is used to avoid Call Back Hell and Pyramid of Doom
-login()
-  .then((loginData) => {
-    console.log("1st .then() :: ", loginData);
-    return fetchData();      // return ပြန်မပေးပါက fetchData() မှ ရသော promise မှ data ကို နောက်ဆက်တွဲ then()/catch() များတွင် parameter ခေါ်ယူ သုံးစွဲနိုင်မည်မဟုတ်ပါ။
-  })
-  .then((dataFromFetchData) => {
-    console.log("2nd .then() :: ", dataFromFetchData);
-    return getUserPhoto();
-  })
-  .then((userPhoto) => {
-    console.log("3rd .then() :: ", userPhoto);
-    return getUserFriends();
-  })
-  .then((userFriends) => {
-    console.log("4th .then() :: ", userFriends);
-    showUI(userFriends);
-  })
-  .catch((error) => {
-    console.log("Inside Catch : ", error);
-  });
+
+// login()
+//   .then((loginData) => {
+//     console.log("1st .then() :: ", loginData);
+//     return fetchData();      // return ပြန်မပေးပါက fetchData() မှ ရသော promise မှ data ကို နောက်ဆက်တွဲ then()/catch() များတွင် parameter ခေါ်ယူ သုံးစွဲနိုင်မည်မဟုတ်ပါ။
+//   })
+//   .then((dataFromFetchData) => {
+//     console.log("2nd .then() :: ", dataFromFetchData);
+//     return getUserPhoto();
+//   })
+//   .then((userPhoto) => {
+//     console.log("3rd .then() :: ", userPhoto);
+//     return getUserFriends();
+//   })
+//   .then((userFriends) => {
+//     console.log("4th .then() :: ", userFriends);
+//     showUI(userFriends);
+//   })
+//   .catch((error) => {
+//     console.log("Inside Catch : ", error);
+//   });
